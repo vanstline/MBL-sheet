2a0aa82826e2334a4ace3a5c67dab3dcce320b4f
Tao <types5755@163.com>
2024-07-09 21:12:32 +0800

    ✨ feat: 去除 fireFox 兼容方案

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ require("./utils/math");
 var _core = require("./core");
 var _polyfill = _interopRequireDefault(require("./utils/polyfill"));
 function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {
-    "default": obj
-  };
+  return obj && obj.__esModule
+    ? obj
+    : {
+        default: obj,
+      };
 }
 // Prevent gulp warning: 'Use of eval is strongly discouraged, as it poses security risks and may cause issues with minification'
 // window.evall = window.eval;
 // polyfill event in firefox
-if (window.addEventListener && navigator.userAgent.indexOf("Firefox") > 0) {
-  (0, _polyfill["default"])();
-}
+// if (window.addEventListener && navigator.userAgent.indexOf("Firefox") > 0) {
+//   (0, _polyfill["default"])();
+// }
 
 // export default MBLsheet;
 
